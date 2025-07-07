fbd86a05de1cb0a998eec40bafd8b609ecad738a
dinozaurka <emilia.kolodko@infotech.edu.pl>
2025-07-07 18:11:16 +0200

    Added a  drop down menu. Changed name.
    Usage: Click on avatar to open. Click on avatar again to close
    
    Disclosure: Links do not work yet. No animation added.

--- src/components/MainHeader/MainHeader.jsx
+++ src/components/MainHeader/MainHeader.jsx
@@ -1,23 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { faCircleUser } from '@fortawesome/free-solid-svg-icons'
 import user_icon_solid_black from "../../images/user_icon_solid_black.png";
 import "./MainHeader.css"
+import DropDownMenu from '../DropDownMenu/DropDownMenu';
 
 function MainHeader() {
+
+  const [open, setOpen] = useState(false);
+
   return (
     <div className='main-header'>
         
         <div className="header-section left">
-            <h1 className='web-title'>Blogeenie</h1>
+            <h1 className='web-title'><a href='#home'>Hob<span className='beet'>BEET</span></a></h1>
             <div className='links'>
-              <a href='#'>Home</a>
-              <a href="#">Friends</a>
+              <a href='#home'>Home</a>
+              <a href="#friends">Friends</a>
               <button className='create-post-btn'>Create</button>
             </div>
         </div>
-        <div className="header-section account">
+        <div className="header-section account" onClick={() => {setOpen(!open)}}>
             <img src={user_icon_solid_black} alt="User Icon"></img>
         </div>
+        <DropDownMenu username={"dinozaurka"} title={"Coffee enthusiast"} isDropDownActive={open}/>
     </div>
   )
 }
